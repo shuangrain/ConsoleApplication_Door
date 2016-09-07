bd7bb1575e351b8f726da5dad271c5b842ce0da7
Minsheng <shuangrain@hotmail.com>
2016-09-07 23:13:39 +0800

    initi

---
+++ ConsoleApplication_Door/ConsoleApplication_Door/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication_Door
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoorController dc = new DoorController();
+            dc.AddDoor(new HorizontalDoor());
+            dc.AddDoor(new VerticalDoor());
+            dc.AddDoor(new AlarmDoor());
+            dc.AddDoor(new AutoAlarmDoor());
+
+            dc.OpenDoor();
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class Door
+    {
+        public virtual void Open()
+        {
+            Console.WriteLine("Open Door");
+        }
+        public virtual void Close()
+        {
+            Console.WriteLine("Close Door");
+        }
+    }
+
+    class HorizontalDoor : Door { }
+
+    class VerticalDoor : Door
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Open vertically");
+        }
+        public override void Close()
+        {
+            Console.WriteLine("Close vertically");
+        }
+    }
+
+    interface IAlarm
+    {
+        void Alert();
+    }
+
+    class Alarm : IAlarm
+    {
+        public void Alert()
+        {
+            Console.WriteLine("Ring ~~");
+        }
+    }
+
+    class AlarmDoor : Door
+    {
+        private IAlarm _alarm;
+
+        public AlarmDoor()
+        {
+            _alarm = new Alarm();
+        }
+
+        public void Alert()
+        {
+            _alarm.Alert();
+        }
+    }
+
+    class AutoAlarmDoor : AlarmDoor
+    {
+        public override void Open()
+        {
+            base.Open();
+            Alert();
+        }
+    }
+
+    class DoorController
+    {
+        protected List<Door> _dootList = new List<Door>();
+
+        public void AddDoor(Door Door)
+        {
+            _dootList.Add(Door);
+        }
+
+        public void OpenDoor()
+        {
+            foreach (var item in _dootList)
+            {
+                item.Open();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# [C#] 利用 interface(介面) abstract(抽象) override(覆寫) inherit(繼承) 實作簡單範例
+
+
+教學：<a href="https://blog.sofast.info/2016/09/c-sharp-use-interface-abstract-override-inheritance-implements-a-simple-example/">https://blog.sofast.info/2016/09/c-sharp-use-interface-abstract-override-inheritance-implements-a-simple-example/</a>
